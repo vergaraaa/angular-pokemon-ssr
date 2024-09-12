b29e990c6ded9d45f1196bc3dcdfb90edc98d89b
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-12 19:15:08 +0200

    ✅ tests on app routes

---
+++ src/app/app.routes.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { routes } from './app.routes';
+import { provideRouter, Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+describe('App Routes', () => {
+  let router: Router;
+  let location: Location;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideRouter(routes)],
+    });
+
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+  });
+
+  it('should navigate to "about" redirect to "/about"', async () => {
+    await router.navigate(['about']);
+
+    expect(location.path()).toBe('/about');
+  });
+
+  it('should navigate to "pokemons/page/1" redirect to "/pokemons/page/1"', async () => {
+    await router.navigate(['pokemons/page/1']);
+
+    expect(location.path()).toBe('/pokemons/page/1');
+  });
+
+  it('should navigate to "unknown-page" redirect to "/about"', async () => {
+    await router.navigate(['unknown-page']);
+
+    expect(location.path()).toBe('/about');
+  });
+});
