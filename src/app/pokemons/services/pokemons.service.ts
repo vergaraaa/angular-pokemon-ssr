a4677025c3dfa169eef5c954041fb2981be466ca
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-14 09:32:30 +0200

    ✅ pokemons service tests

--- src/app/pokemons/services/pokemons.service.ts
+++ src/app/pokemons/services/pokemons.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 import { Pokemon, PokemonAPIResponse, SimplePokemon } from '../interfaces';
 
 @Injectable({
@@ -33,6 +33,20 @@ export class PokemonsService {
   }
 
   public loadPokemon(id: string): Observable<Pokemon> {
-    return this.http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    return this.http
+      .get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.log('An error occurred:', error.error);
+    } else {
+      console.log(`Backend return code ${error.status}, body: ${error.error}`);
+    }
+
+    const errorMessage = error.error ?? 'Error occurred';
+
+    return throwError(() => new Error(errorMessage));
   }
 }
